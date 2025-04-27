71b62e483a2683fb3d14fc5c9e48034701853422
MaheshBBhor <maheshbhor97@gmail.com>
2025-04-27 13:44:30 +0530

    feat

--- youtube/src/components/utils/constant.js
+++ youtube/src/components/utils/constant.js
@@ -1,7 +1,2 @@
-// const GOOGLE_API_KEY = "AIzaSyAmlsTyc6mVvJCXtakkALOgoYhw19fv6eg";
-// export const YOU_TUBE_API_VIDEO =
-//   " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc%2Cc0KYU2j0TM4%2CeIho2S0ZahI&key=" +
-//   GOOGLE_API_KEY;
-
 export const YOU_TUBE_API_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyCjFodfD1bzMlQkk9gdWCyHwYzWBePjOSo";
